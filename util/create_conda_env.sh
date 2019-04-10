2021978b905c4697dc9f1cbc1569bb2f25002da9
Pablo Moreno <pablo.a.moreno@gmail.com>
2019-04-10 11:21:53 +0100

    Adds utility for creating conda environments if they don't exist.

---
+++ util/create_conda_env.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+conda_env_name=${CONDA_ENV:-$1}
+conda_packages=${CONDA_PACKAGES:-$2}
+
+conda env list | grep $conda_env_name
+
+if [ $? -eq 1 ]; then
+  # conda environment doesn't exist, create it
+  conda create -y -n $conda_env_name $conda_packages
+fi
+# we currently trust that if the environment with that name is created, then it contains what we need.
+
