b815f88da4af26b27157c6eb10b373d5a4101e04
Jonathan Manning <jmanning@ebi.ac.uk>
2019-04-17 13:48:59 +0100

    Improved parameterisation

--- exec/submitControlWorkflow.sh
+++ exec/submitControlWorkflow.sh
@@ -1,8 +1,40 @@
 #!/usr/bin/env bash
 
+usage() { echo "Usage: $0 [-e <experiment ID>] [-s <skip quantification, yes or no>] [-t <tertiary workflow>] [-o <overwrite exising results, yes or no>]"  1>&2; }  
+
+e=
+s=no
+t=none
+o=no
+
+while getopts ":e:s:t:o:" o; do
+    case "${o}" in
+        e)
+            e=${OPTARG}
+            ;;
+        s)
+            s=${OPTARG}
+            ;;
+        t)
+            t=${OPTARG}
+            ;;
+        o)
+            o=${OPTARG}
+            ;;
+        *)
+            usage
+            exit 0
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
 # Trivially simple script to trigger SCXA workflow
 
-expName=${1:-''}
+expName=$e
+skipQuantification=$s
+tertiaryWorflow=$t
+overwrite=$o
 
 workflow=scxa-control-workflow
 
@@ -26,11 +58,35 @@ export SCXA_BRANCH=$scxaBranch
 
 # Build the Nextflow command
 
+expNamePart=
 if [ -n "$expName" ]; then
     expNamePart="--expName $expName"
 fi
 
-nextflowCommand="NXF_VER=19.03.0-SNAPSHOT nextflow run -N $SCXA_REPORT_EMAIL -r $scxaBranch -resume ${workflow} $expNamePart --enaSshUser fg_atlas_sc --sdrfDir $SCXA_SDRF_DIR -work-dir $workingDir"
+skipQuantificationPart=
+if [ -n "$skipQuantification" ]; then
+    if [ "$skipQuantification" != 'yes' ] && [ "$skipQuantification" != 'no' ]; then
+        echo "Skip quantification (-s) must be 'yes' or 'no'"
+    fi
+
+    skipQuantificationPart="--skipQuantification $skipQuantification"
+fi
+
+overwritePart=
+if [ -n "$overwrite" ]; then
+    if [ "$overwrite" != 'yes' ] && [ "$overwrite" != 'no' ]; then
+        echo "Override (-o) must be 'yes' or 'no'"
+    fi
+
+    overwritePart="--overwrite $overwrite"
+fi
+
+tertiaryWorkflowPart=
+if [ -n "$tertiaryWorkflow" ]; then
+    tertiaryWorkflowPart="--tertiaryWorkflow $tertiaryWorkflow"
+fi
+
+nextflowCommand="NXF_VER=19.03.0-SNAPSHOT nextflow run -N $SCXA_REPORT_EMAIL -r $scxaBranch -resume ${workflow} $expNamePart $skipQuantificationPart $tertiaryWorkflowPart $overwritePart --enaSshUser fg_atlas_sc --sdrfDir $SCXA_SDRF_DIR -work-dir $workingDir"
 
 # Run the LSF submission if it's not already running
 
