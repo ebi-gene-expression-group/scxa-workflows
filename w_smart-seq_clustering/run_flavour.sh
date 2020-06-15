63ea319f3f9629fda5af41fe8410eca2ee026a6f
Jonathan Manning <jmanning@ebi.ac.uk>
2020-06-15 15:21:23 +0100

    Remove software dependency handling, remove historical artifacts

--- w_smart-seq_clustering/run_flavour.sh
+++ w_smart-seq_clustering/run_flavour.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 
-# Create needed conda environments if not available
-if [ ! -z ${create_conda_env+x} ]; then
-  create_conda_env.sh _bioblend@0.12.0_py3 'bioblend=0.12.0 requests-toolbelt=0.9.1' bioconda
-  source activate _bioblend@0.12.0_py3
-fi
-
 which run_galaxy_workflow.py
 if [ $? -gt 0 ]; then
-  echo "run_galaxy_workflow.py is not in the path, exiting"
+  echo "run_galaxy_workflow.py is not in the path, please install galaxy-workflow-executor. Exiting"
   exit 1
 fi
 
@@ -20,6 +14,7 @@ fi
 
 set -e
 echo "Results will be available on $WORKDIR"
+
 # Main clustering run
 run_galaxy_workflow.py -C $GALAXY_CRED_FILE \
                        -i $inputs_yaml \
@@ -30,17 +25,5 @@ run_galaxy_workflow.py -C $GALAXY_CRED_FILE \
                        -a $allowed_errors \
                        -G $GALAXY_INSTANCE $ADDITIONAL_GALAXY_WF_EXECUTOR_OPTION
 mv $WORKDIR/software_versions_galaxy.txt $WORKDIR/clustering_software_versions.txt
-# Filter TPM if needed
-if [ $tpm_filtering = "True" ]; then
-  run_galaxy_workflow.py -C $GALAXY_CRED_FILE \
-                         -i $inputs_tpm_filtering_yaml \
-                         -o $WORKDIR \
-                         -W $tpm_filtering_workflow_definition \
-                         -P $params_tpm_filtering_json \
-                         -H scanpy-tpm-filtering-$EXP_ID \
-                         -G $GALAXY_INSTANCE $ADDITIONAL_GALAXY_WF_EXECUTOR_OPTION
-
-  mv $WORKDIR/software_versions_galaxy.txt $WORKDIR/filter_tpms_software_versions.txt
-fi
 
 choose_resolution_per_clustering.py --clusters-path $WORKDIR --output-dir $WORKDIR
--- w_smart-seq_clustering/setup.sh
+++ w_smart-seq_clustering/setup.sh
@@ -1,18 +1,8 @@
 #!/usr/bin/env bash
 
 scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-source $scriptDir/../util/manifest_handling.sh
 
 [ -z ${envs_for_workflow_run+x} ] && echo "Env var envs_for_workflow_run should be set by upstream script." && exit 1
-
-# Create inputs.yaml based on MANIFEST
-if [ ! -z ${EXP_BUNDLE} ]; then
-  echo "Using Bundle to set inputs"
-  matrix_file=$EXP_BUNDLE/$( file_for_desc_param $EXP_BUNDLE/MANIFEST 'mtx_matrix_content' 'raw' )
-  genes_file=$EXP_BUNDLE/$( file_for_desc_param $EXP_BUNDLE/MANIFEST 'mtx_matrix_rows' 'raw' )
-  barcodes_file=$EXP_BUNDLE/$( file_for_desc_param $EXP_BUNDLE/MANIFEST 'mtx_matrix_cols' 'raw' )
-  gtf_file=$REF_DIR/$EXP_SPECIE/$( file_for_desc_param $EXP_BUNDLE/MANIFEST 'reference_annotation' '' )
-fi
 
 [ -z ${matrix_file+x} ] && echo "Env var matrix_file should be set." && exit 1
 [ -z ${genes_file+x} ] && echo "Env var genes_file should be set." && exit 1
@@ -31,43 +21,5 @@ sed "s+<MATRIX_PATH>+$matrix_file+" $scriptDir/scanpy_clustering_inputs.yaml.tem
 
 echo "export inputs_yaml=$inputs_yaml" > $envs_for_workflow_run
 echo "export workflow_definition=$scriptDir/scanpy_clustering_workflow.json" >> $envs_for_workflow_run
-echo "export params_json=$scriptDir/scanpy_clustering_params.json" >> $envs_for_workflow_run
+echo "export params_json=$scriptDir/scanpy_clustering_workflow_parameters.yaml" >> $envs_for_workflow_run
 echo "export allowed_errors=$scriptDir/scanpy_clustering_allowed_errors.yaml" >> $envs_for_workflow_run
-
-# Results potentially relevant for TPM filtering:
-raw_filtered_genes=$WORKDIR/raw_filtered_genes.tsv
-raw_filtered_barcodes=$WORKDIR/raw_filtered_barcodes.tsv
-
-# See if we need to filter TPMs (in the future this could depend on the protocol)
-if [ ! -z ${EXP_BUNDLE} ]; then
-  tpm_matrix_file=$( file_for_desc_param $EXP_BUNDLE/MANIFEST 'mtx_matrix_content' 'tpm' )
-fi
-
-tpm_filtering=False
-if [ ${#tpm_matrix_file} -gt 0 ]; then
-  # matrix file exists for tpm
-  if [ ! -z ${EXP_BUNDLE} ]; then
-    tpm_matrix_file=${tpm_matrix:-$EXP_BUNDLE/$tpm_matrix_file}
-    tpm_genes_file=${tpm_genes:-$EXP_BUNDLE/$( file_for_desc_param $EXP_BUNDLE/MANIFEST 'mtx_matrix_rows' 'tpm')}
-    tpm_barcodes_file=${tpm_barcodes:-$EXP_BUNDLE/$( file_for_desc_param $EXP_BUNDLE/MANIFEST 'mtx_matrix_cols' 'tpm' )}
-  fi
-
-  [ -z ${tpm_matrix_file+x} ] && echo "Env var tpm_matrix_file should be set." && exit 1
-  [ -z ${tpm_genes_file+x} ] && echo "Env var tpm_genes_file should be set." && exit 1
-  [ -z ${tpm_barcodes_file+x} ] && echo "Env var tpm_barcodes_file should be set." && exit 1
-
-  tpm_filtering=True
-
-  inputs_tpm_filtering_yaml=$WORKDIR/scanpy_tpm_filtering_inputs_$EXP_ID\.yaml
-  sed "s+<MATRIX_PATH>+$tpm_matrix_file+" $scriptDir/scanpy_tpm_filtering_inputs.yaml.template | \
-      sed "s+<GENES_PATH>+$tpm_genes_file+" | \
-      sed "s+<BARCODES_PATH>+$tpm_barcodes_file+" | \
-      sed "s+<FILTERED_GENES>+$raw_filtered_genes+" | \
-      sed "s+<FILTERED_BARCODES>+$raw_filtered_barcodes+" > $inputs_tpm_filtering_yaml
-
-  echo "export inputs_tpm_filtering_yaml=$inputs_tpm_filtering_yaml" >> $envs_for_workflow_run
-  echo "export tpm_filtering_workflow_definition=$scriptDir/scanpy_tpm_filtering_workflow.json" >> $envs_for_workflow_run
-  echo "export params_tpm_filtering_json=$scriptDir/scanpy_tpm_filtering_params.json" >> $envs_for_workflow_run
-fi
-
-echo "export tpm_filtering=$tpm_filtering" >> $envs_for_workflow_run
