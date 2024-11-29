78645d218f2a176ffd200b67ab76abe08ddd0e08
Anil Thanki <thanki.anil@gmail.com>
2024-11-29 16:08:45 +0000

    Update run_tertiary_workflow.sh - dataprep to data_prep

--- bin/run_tertiary_workflow.sh
+++ bin/run_tertiary_workflow.sh
@@ -71,7 +71,7 @@ else
 fi
 
 # Prepare input data for tertiary workflow stores them in $SCXA_WORKDIR 
-bash $baseDir/w_tertiary/scripts/dataprep.sh $EXP_ID $SCXA_WORKDIR
+bash $baseDir/w_tertiary/scripts/data_prep.sh $EXP_ID $SCXA_WORKDIR
 
 # Run the workflow
 nextflow run $baseDir/scxa-tertiary-workflow/main.nf \
