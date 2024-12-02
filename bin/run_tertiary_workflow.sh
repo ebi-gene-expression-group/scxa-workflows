a9344f628c6e510bceb69d7a63b334fea8459ab3
Anil Thanki <thanki.anil@gmail.com>
2024-12-02 15:12:06 +0000

    Update run_tertiary_workflow.sh - changes the way input files gathered

--- bin/run_tertiary_workflow.sh
+++ bin/run_tertiary_workflow.sh
@@ -71,7 +71,10 @@ else
 fi
 
 # Prepare input data for tertiary workflow stores them in $SCXA_WORKDIR 
-bash $baseDir/w_tertiary/scripts/data_prep.sh $EXP_ID $SCXA_WORKDIR
+# bash $baseDir/w_tertiary/scripts/data_prep.sh $EXP_ID $SCXA_WORKDIR
+
+mkdir $SCXA_WORKDIR/$EXP_ID
+cp $matrix_file $genes_file $barcodes_file $cell_meta_file $gene_meta_file $SCXA_WORKDIR/$EXP_ID
 
 # Run the workflow
 nextflow run $baseDir/scxa-tertiary-workflow/main.nf \
